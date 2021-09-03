87e7e592ae00c5db001623e834c2dd97535881f0
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2021-09-04 00:50:10 +0800

    Declared boardState (2D array), moves (int), player (char)

--- tictactoe-page/script.js
+++ tictactoe-page/script.js
@@ -5,6 +5,12 @@ let boardState = [
     []
 ]
 
+let moves = 0;
+// assume X first for now
+let player = 'X';
+
+console.log(boardState)
+
 boxes.forEach(box => {
     box.addEventListener('click', () => {
         console.log("User clicked on", box.id)
