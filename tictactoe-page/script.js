984e43574270cc0276b0ddd102a272ac1c290a8d
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2021-09-04 01:17:34 +0800

    Added if-else to check if a clicked box is still empty (if empty, set innerhtml to player symbol and moves++)

--- tictactoe-page/script.js
+++ tictactoe-page/script.js
@@ -15,7 +15,23 @@ boxes.forEach(box => {
     box.addEventListener('click', () => {
         console.log("User clicked on", box.id)
         console.log("That is row", box.id[6], "and col", box.id[7])
-        let row = box.id[6];
-        let col = box.id[7];
+
+        // check if box is already filled
+ 
+        if (!(box.innerHTML === 'X' || box.innerHTML === 'O')) {
+            if (moves % 2 === 0) {
+                // impending first move
+                // set player symbol on box
+                box.innerHTML = 'X';
+                // register move
+                moves++;
+            } else {
+                // other player's turn
+                // set player symbol on box
+                box.innerHTML = 'O';
+                // register move
+                moves++;
+            }
+        }
     })
 })
