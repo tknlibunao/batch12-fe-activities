92d3f5a2ed6eeca55907306126b0e8975357200a
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2021-09-04 09:04:12 +0800

    Queried for #options, hide options once button X/O is clicked

--- tictactoe-page/script.js
+++ tictactoe-page/script.js
@@ -1,5 +1,6 @@
 let boxes = document.querySelectorAll('.box');
 let buttons = document.querySelectorAll('button');
+let options = document.querySelector('#options');
 let moves = 0;
 let firstPlayer, secondPlayer = '';
 let boardState = [ [], [], [] ];
@@ -7,36 +8,25 @@ let boardState = [ [], [], [] ];
 buttons.forEach(button => {
     button.addEventListener('click', () => {
        firstPlayer = button.innerHTML;
-       if (firstPlayer === 'X') {
-           secondPlayer = 'O';
-       } else {
-           secondPlayer = 'X';
-       }
+       secondPlayer = firstPlayer === 'X' ? 'O' : 'X';
+       options.classList.add("hidden");
+       boxes.forEach(box => {
+           box.style.cursor = "pointer";
+       })
     })
 })
 
 
 boxes.forEach(box => {
     box.addEventListener('click', () => {
-        console.log("User clicked on", box.id)
-        console.log("That is row", box.id[6], "and col", box.id[7])
-
-        // check if box is already filled
- 
-        if (!(box.innerHTML === 'X' || box.innerHTML === 'O')) {
-            if (moves % 2 === 0) {
-                // impending first move
-                // set firstplayer symbol on box
-                box.innerHTML = firstPlayer;
-                // register move
-                moves++;
-            } else {
-                // other player's turn
-                // set player symbol on box
-                box.innerHTML = secondPlayer;
-                // register move
+        // check if player has been selected
+        if (firstPlayer.length > 0) {
+            // check if box is already filled
+            if (!(box.innerHTML === 'X' || box.innerHTML === 'O')) {
+                box.innerHTML = moves % 2 === 0 ? firstPlayer : secondPlayer;
                 moves++;
             }
         }
+        
     })
 })
