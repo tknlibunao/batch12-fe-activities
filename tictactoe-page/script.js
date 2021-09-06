a66d39a9ed97469ba38a409c4fe630901350b0a6
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2021-09-06 20:32:26 +0800

    Renamed let moves -> moveCount; deleted let charChosen, finish, moveHistory, dummy

--- tictactoe-page/script.js
+++ tictactoe-page/script.js
@@ -4,56 +4,21 @@ let divOptions = document.querySelector('#options');
 let divAnnounce = document.querySelector('#announce');
 
 // State variables
-let moves = 0;
-let charChosen, finish = false;
+let moveCount = 0;
 let firstPlayer, secondPlayer = '';
-let boardState = [ [], [], [] ];
-let moveHistory = [];
-let dummy = [];
+let boardState = [];
+
+// For now, game starts when user chooses a character (presses 'X' or 'O' button)
 charBtns.forEach(button => {
     button.addEventListener('click', () => {
-        charChosen = true;
-        firstPlayer = button.innerHTML;
-        secondPlayer = firstPlayer === 'X' ? 'O' : 'X';
-        divAnnounce.innerHTML = `${firstPlayer}'s turn`;
-        divOptions.innerHTML = `<div id="history">
-                                    <div class="left">
-                                        <button class='char-move' disabled>Prev</button>
-                                    </div>
-                                    <div class="right">
-                                        <button class='char-move' disabled>Next</button>
-                                    </div>
-                                </div>`
+        setFirstPlayer(button);
+        
         boxes.forEach(box => {
             box.style.cursor = "pointer";
-        })  
-    })
-})
+            box.addEventListener('click', userClickedBox)
+        })
 
 
-boxes.forEach(box => {
-    box.addEventListener('click', () => {
-        let row = box.id[6];
-        let col = box.id[7];
-
-        // check if player has been selected
-        if (charChosen) {
-            // check if box is already filled
-            if (!(box.innerHTML === 'X' || box.innerHTML === 'O')) {
-                box.innerHTML = moves % 2 === 0 ? firstPlayer : secondPlayer;
-                divAnnounce.innerHTML = moves % 2 === 0 ? `${secondPlayer}'s turn` : `${firstPlayer}'s turn`
-                boardState[row][col] = box.innerHTML;
-            } else {
-                console.log("Cell already filled!")
-            }
-            let dummy = boardState.map(arr => {
-                return arr.slice()
-            })
-            moveHistory.push(dummy);
-            moves++;
-        } else {
-            console.log("Choose character first!")
-        }
     })
 })
 
